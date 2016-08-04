bd410bb159bcb09e35ff0a41f32c9af197e47567
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-08-04 22:38:01 +0200

    Changed HTTP response content type to HTML.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<a href="/about">About</a><br/>');
   res.write('<a href="/blog">Blog</a><br/>');
   res.end();
